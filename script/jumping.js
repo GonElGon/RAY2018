9b5005bf4739800cfb5a58948445b243eed6c0cf
GonElGon <44227294+GonElGon@users.noreply.github.com>
2019-01-09 12:20:07 +0000

    Add files via upload

--- script/jumping.js
+++ script/jumping.js
@@ -1,11 +1,40 @@
-console.log("Cargando juegos");
+var game_canvas;
+var game_context;
 
-/*
- *
- * game_state, definirá los estados del juego siendo:
- *   - [0] estado inicial, juego parado
- *   - [1] estado principal, juego en ejecución
- *   - [2] estado de pausa, esperando a presionar s para iniciar o reanudar
- *   - [3] estado final, juego parado
- *
- */
+var cloudx;
+
+setInterval(updateGame, 10);
+
+function initGame () {
+	game_canvas = document.getElementById("gameCanvas");
+	game_context = game_canvas.getContext("2d");
+
+	
+	cloudx = game_canvas.width;
+
+}
+
+function updateGame () {
+	game_context.clearRect(0, 0, game_canvas.width, game_canvas.height);
+
+	game_context.fillStyle = "green";
+
+	game_context.fillRect(50, 50, 50, 50);
+	
+	game_context.fillStyle = "red";
+
+	game_context.fillRect(40, (game_canvas.height - 100 ), 50, 50);
+	
+	game_context.fillStyle = "blue";
+
+	game_context.fillRect((game_canvas.width / 2) + 50, (game_canvas.height / 2 ), 50, 50);
+
+	game_context.fillStyle = "white";
+
+	cloudx = cloudx - 20
+
+	game_context.fillRect(cloudx, 50, 50, 50);
+
+
+
+}
