857c422a60c7873cf04b18348aee00418f1a7704
GonElGon <44227294+GonElGon@users.noreply.github.com>
2019-01-09 08:59:27 +0000

    Add files via upload

---
+++ script/jumping.js
@@ -0,0 +1,11 @@
+console.log("Cargando juegos");
+
+/*
+ *
+ * game_state, definirá los estados del juego siendo:
+ *   - [0] estado inicial, juego parado
+ *   - [1] estado principal, juego en ejecución
+ *   - [2] estado de pausa, esperando a presionar s para iniciar o reanudar
+ *   - [3] estado final, juego parado
+ *
+ */
